91d5a775174a52475c9e5d4f8c7c3fef8c5d35ee
Khoa Le <khoaln6@gmail.com>
2020-05-03 14:02:04 +0700

    Fix prisma client not in context

--- pages/api/directory/city.js
+++ pages/api/directory/city.js
@@ -1,19 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import useAppContext from "../../../utils/middleware/appcontext";
 
-const prisma = new PrismaClient();
-
 const City = async (req, res) => {
   const {
-    query: { region_id }
+    query: { region_id },
   } = req;
   if (region_id == "") {
     return res.json([]);
   }
+  const prisma = new PrismaClient();
   const cities = await prisma.directory_city.findMany({
     where: {
-      region_id: parseInt(region_id)
-    }
+      region_id: parseInt(region_id),
+    },
   });
   res.json(cities);
 };
--- pages/api/directory/region.js
+++ pages/api/directory/region.js
@@ -1,13 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import useAppContext from "../../../utils/middleware/appcontext";
 
-const prisma = new PrismaClient();
-
 const Region = async (req, res) => {
+  const prisma = new PrismaClient();
   const regions = await prisma.directory_region.findMany({
     where: {
-      country_id: "VN"
-    }
+      country_id: "VN",
+    },
   });
   res.json(regions);
 };
